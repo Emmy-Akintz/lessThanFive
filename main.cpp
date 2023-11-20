07b3332a2e79726dbb9ca6cddf202e9294d9ee33
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-20 21:30:04 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-// #include <math.h>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    int randomNum = 5;
+    int randomNum = rand() %11;
+    cout << randomNum << endl;
 
     for (int i = 0; i < 5; i++)
     {
         int userNum;
-        cout << "Input a number that must be less than the random number and you have five attempts!";
+        cout << "Input a number that must be less than the random number and you have five attempts!" << endl;
         cout << "Input a number" << endl;
         cin >> userNum;
 
