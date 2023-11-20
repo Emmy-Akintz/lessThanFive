5d72b5f3159d386588e574a35cadec3fa128073a
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-20 22:56:11 +0100

    update

--- main.cpp
+++ main.cpp
@@ -6,27 +6,38 @@ using namespace std;
 
 int main()
 {
-    srand((unsigned) time(NULL));
+    srand((unsigned)time(NULL));
 
-    int randomNum = rand() %10;
+    int randomNum = rand() % 10000;
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 20; i++)
     {
         int userNum;
-        cout << "Input a number that must be less than the random number which is less than or equal to 10 and you have five attempts!" << endl;
+        cout << "Input a number that must be less than the random number which is less than or equal to 10000 and you have 20 attempts!" << endl;
         cout << "Input a number" << endl;
         cin >> userNum;
 
-        if (userNum < randomNum) {
+        if (userNum < randomNum)
+        {
             cout << "The number is less than the random number." << endl;
-        } else if (userNum > randomNum) {
+        }
+        else if (userNum > randomNum)
+        {
             cout << "The number is greater than the random number." << endl;
-        } else if ( (userNum = randomNum) ) {
+        }
+        else if ((userNum = randomNum))
+        {
             cout << "You have guessed right and the value is " << randomNum << endl;
             break;
-        } else {
+        }
+        else
+        {
+            cout << "You lost!" << endl;
+            cout << "The value is " << randomNum << endl;
             return 0;
         }
     }
+
+         
     return 0;
 }
